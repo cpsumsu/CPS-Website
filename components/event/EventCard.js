d9419155abdd4d10cae9cfcd8abc89a69e12f447
K4Lok <samkalok107@gmail.com>
2022-12-17 19:21:57 +0800

    feature: animated skeleton placeholder for image when loading

--- components/event/EventCard.js
+++ components/event/EventCard.js
@@ -1,11 +1,10 @@
 import Image from "next/image"
+import BlurImage from "./BlurImage"
 
 export default function EventCard({event, tagColor}) {
   return (
     <div className='relative rounded-lg shadow-lg dark:shadow-md dark:shadow-slate-800 overflow-hidden hover:scale-[105%] hover:shadow-md duration-300'>
-      <Image src={event.imageUrl} width={300} height={300} alt={event.name}
-        className="object-cover w-64 h-64"
-      />
+      <BlurImage src={event.imageUrl} width={300} height={300} alt={event.name}/>
       {/* Card Content */}
       <div className="px-6 py-4 space-y-1 bg-gray-100 dark:bg-cardDark">
         <p className="inline-block text-sm font-bold text-white px-2 py-1 rounded-md" style={{backgroundColor: tagColor[event.type]}}>{event.type}</p>
