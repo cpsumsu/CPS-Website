6b68ac35ce9c8e2e536087e745d67653b707f058
K4Lok <samkalok107@gmail.com>
2022-12-27 10:07:18 +0800

    update Event Card description background

--- components/event/EventCard.js
+++ components/event/EventCard.js
@@ -12,7 +12,7 @@ export default function EventCard({event, width}) {
     <div className='relative rounded-lg shadow-lg dark:shadow-md dark:shadow-slate-800 overflow-hidden md:hover:scale-[105%] md:hover:shadow-md duration-300' style={{width: `${width}px`}}>
       <BlurImage src={event.imageUrl} width={width} height={300} alt={event.name}/>
       {/* Card Content */}
-      <div className="px-4 py-4 space-y-1 bg-gray-100 dark:bg-cardDark duration-300">
+      <div className="px-4 py-4 space-y-1 bg-white dark:bg-cardDark duration-300">
         <p className="inline-block text-sm font-bold text-white px-2 py-1 rounded-md" style={{backgroundColor: tagColor[event.type]}}>{event.type}</p>
         <p className="text-lg font-bold dark:text-white truncate duration-300">{event.name}</p>
         <p className="text-sm font-semibold text-neutral-400 truncate">{event.leader ?? EMPTY}</p>
