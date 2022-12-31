f5a34b533ddc38c1dd9dd027759c090ad855ff3e
K4Lok <samkalok107@gmail.com>
2023-01-01 05:38:16 +0800

    feature: light theme is now storing to the local device

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,25 @@
+import { useState, useEffect } from 'react';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
-import { useState } from 'react';
 
 export default function Layout(props) {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(null);
 
+  useEffect(() => {
+    const isDark = JSON.parse(localStorage.getItem('theme-is-dark'));
+    if (isDark === undefined || isDark === null) {
+      setDarkMode(false);
+      return;
+    }
+
+    setDarkMode(isDark);
+  }, [])
+
+  useEffect(() => {
+    if (darkMode === null) return;
+    localStorage.setItem('theme-is-dark', JSON.stringify(darkMode));
+  }, [darkMode]);
+  
   const handleThemeToggle = () => {
     setDarkMode(prev => !prev);
   }
